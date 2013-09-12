228fac55d42d2733f64382e2f0200e9a3efee940
Charles Feng <charles@charles.io>
2013-09-12 12:29:25 -0700

    serve all assets bundled together

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
+//= require jquery.js
+//= require jquery-ui.js
+//= require underscore.js
+//= require gmap3.js
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
